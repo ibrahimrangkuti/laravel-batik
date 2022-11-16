7786bec613802491d48e4d2668c247add5897b1f
ibrahimrangkuti <rangkutyibrahim765@gmail.com>
2022-11-16 16:20:25 +0700

    memperbaiki assets di main layout

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -19,6 +19,7 @@
     <script
         src="{{ asset('assets/js/bootstrap.min.js') }}tps://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js">
     </script>
+    <script src="{{ asset('assets/js/bootstrap.bundle.min.js') }}"></script>
 </body>
 
 </html>
